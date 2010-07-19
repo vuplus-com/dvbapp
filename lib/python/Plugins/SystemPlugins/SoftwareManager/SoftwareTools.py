fd8c95cfc3e21f18ae8322ea30e4a387bc08589b
ikseong <ikseong@vuplus>
2010-07-19 10:57:01 +0900

    tpm block when model dm8000

--- lib/python/Plugins/SystemPlugins/SoftwareManager/SoftwareTools.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/SoftwareTools.py
@@ -100,7 +100,7 @@ class SoftwareTools(DreamInfoHandler):
 
 	def getUpdates(self, callback = None):
 		if SoftwareTools.lastDownloadDate is None:
-			if  self.hardware_info.device_name != "dm7025":
+			if  self.hardware_info.device_name != "dm8000":
 				rootkey = ['\x9f', '|', '\xe4', 'G', '\xc9', '\xb4', '\xf4', '#', '&', '\xce', '\xb3', '\xfe', '\xda', '\xc9', 'U', '`', '\xd8', '\x8c', 's', 'o', '\x90', '\x9b', '\\', 'b', '\xc0', '\x89', '\xd1', '\x8c', '\x9e', 'J', 'T', '\xc5', 'X', '\xa1', '\xb8', '\x13', '5', 'E', '\x02', '\xc9', '\xb2', '\xe6', 't', '\x89', '\xde', '\xcd', '\x9d', '\x11', '\xdd', '\xc7', '\xf4', '\xe4', '\xe4', '\xbc', '\xdb', '\x9c', '\xea', '}', '\xad', '\xda', 't', 'r', '\x9b', '\xdc', '\xbc', '\x18', '3', '\xe7', '\xaf', '|', '\xae', '\x0c', '\xe3', '\xb5', '\x84', '\x8d', '\r', '\x8d', '\x9d', '2', '\xd0', '\xce', '\xd5', 'q', '\t', '\x84', 'c', '\xa8', ')', '\x99', '\xdc', '<', '"', 'x', '\xe8', '\x87', '\x8f', '\x02', ';', 'S', 'm', '\xd5', '\xf0', '\xa3', '_', '\xb7', 'T', '\t', '\xde', '\xa7', '\xf1', '\xc9', '\xae', '\x8a', '\xd7', '\xd2', '\xcf', '\xb2', '.', '\x13', '\xfb', '\xac', 'j', '\xdf', '\xb1', '\x1d', ':', '?']
 				etpm = eTPM()
 				l2cert = etpm.getCert(eTPM.TPMD_DT_LEVEL2_CERT)
@@ -121,7 +121,7 @@ class SoftwareTools(DreamInfoHandler):
 					return
 				val = etpm.challenge(rnd)
 				result = decrypt_block(val, l3key)
-			if self.hardware_info.device_name == "dm7025" or result[80:88] == rnd:
+			if self.hardware_info.device_name == "dm8000" or result[80:88] == rnd:
 				if SoftwareTools.NetworkConnectionAvailable == True:
 					SoftwareTools.lastDownloadDate = time()
 					if SoftwareTools.list_updating is False and callback is None:
