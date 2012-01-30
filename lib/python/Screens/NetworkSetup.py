b3b8f93a1266f44297145a1656a87037d64ee007
Chang.H.S <jhs@dev3>
2012-01-30 18:50:42 +0900

    NetworkSetup.py : fix checkNetworkCB function to prevent crash error.

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -960,20 +960,22 @@ class AdapterSetupConfiguration(Screen, HelpableScreen):
 						iNetwork.checkNetworkState(self.checkNetworkCB)
 
 	def checkNetworkCB(self,data):
-		if iNetwork.getAdapterAttribute(self.iface, "up") is True:
-			if self.LinkState is True:
-				if data <= 2:
-					self["statuspic"].setPixmapNum(0)
+		try:
+			if iNetwork.getAdapterAttribute(self.iface, "up") is True:
+				if self.LinkState is True:
+					if data <= 2:
+						self["statuspic"].setPixmapNum(0)
+					else:
+						self["statuspic"].setPixmapNum(1)
+					self["statuspic"].show()
 				else:
 					self["statuspic"].setPixmapNum(1)
-				self["statuspic"].show()	
+					self["statuspic"].show()
 			else:
 				self["statuspic"].setPixmapNum(1)
 				self["statuspic"].show()
-		else:
-			self["statuspic"].setPixmapNum(1)
-			self["statuspic"].show()
-
+		except:
+			pass
 
 class NetworkAdapterTest(Screen):	
 	def __init__(self, session,iface):
